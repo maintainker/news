d0b8ac26026ab2d1de52f82ef31b27b116613134
maintainker <56953458+maintainker@users.noreply.github.com>
2020-02-23 12:03:22 +0900

    Add files via upload

---
+++ precache-manifest.d681f6b39d4e2014f6487071b38c5f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ac2bce4eb4776070f321bd0cf016ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e13bad4cf5da465514",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a6d26978d695d2f95b67",
+    "url": "/static/js/2.5e95e931.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5e95e931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e13bad4cf5da465514",
+    "url": "/static/js/main.cdb3e5e6.chunk.js"
+  },
+  {
+    "revision": "9f8e5a9bb1fa0fd9050c",
+    "url": "/static/js/runtime-main.fb0b9274.js"
+  }
+]);
